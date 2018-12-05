143f341b918dd9935e1a394e54e0bb479c8ae5b1
Tania Balbuena <tbalbuenaa@gmail.com>
2018-12-05 22:44:26 +0000

    tried adding conditionals

--- js/script.js
+++ js/script.js
@@ -13,7 +13,16 @@ $("#shoot").click(function(){
     userChoice=$("#input").val();
     $("#userChoice").text(userChoice);
     var randomNumber = Math.random();
-    
+    var computerChoice= Math.random();
+        if (computerChoice < 0.3){
+            $("#computerChoice").text("rock");
+        } 
+        else if (computerChoice < 0.66){
+            $("#computerChoice").text("paper");
+        }
+        else if (computerChoice < 1){
+            $("#computerChoice").text("scissors");
+        }
 });
 
 
